1568e457c865aa7e202d4d074bc0301951fa1e4b
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 18:58:19 +0100

    Update day-05.sql

--- day-05.sql
+++ day-05.sql
@@ -5,8 +5,7 @@
 -- Question:
 -- Some elves took time off after the holiday rush, but not everyone has returned to work. List all elves by name, showing their return date. If they have not returned from vacation, list their return date as "Still resting".
 --
--- Some elves took time off after the holiday rush, but not everyone has returned to work. List all elves by name, showing their return date. If they have not returned from vacation, list their return date as "Still resting".
---
+
 
 -- Table Schema:
 -- Table: elves
