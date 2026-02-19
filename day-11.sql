dc567eca4e9a51cad81d587da47b8b2a01a8385d
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 19:00:07 +0100

    Update day-11.sql

--- day-11.sql
+++ day-11.sql
@@ -5,8 +5,6 @@
 -- Question:
 -- At the winter market, Cindy Lou is browsing the clothing inventory and wants to find all items with "sweater" in their name. But the challenge is the color and item columns have inconsistent capitalization. Can you write a query to return only the sweater names and their cleaned-up colors.
 --
--- At the winter market, Cindy Lou is browsing the clothing inventory and wants to find all items with "sweater" in their name. But the challenge is the color and item columns have inconsistent capitalization. Can you write a query to return only the sweater names and their cleaned-up colors.
---
 
 -- Table Schema:
 -- Table: winter_clothing
