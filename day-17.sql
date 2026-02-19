484fe5d7471a996166dbef25e0a345165f89ba5f
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 19:01:50 +0100

    Update day-17.sql

--- day-17.sql
+++ day-17.sql
@@ -5,8 +5,7 @@
 -- Question:
 -- During a quiet evening of reflection, Cindy Lou wants to categorize her tasks based on how peaceful they are. Can you write a query that adds a new column classifying each task as 'Calm' if its noise_level is below 50, and 'Chaotic' otherwise?
 --
--- During a quiet evening of reflection, Cindy Lou wants to categorize her tasks based on how peaceful they are. Can you write a query that adds a new column classifying each task as 'Calm' if its noise_level is below 50, and 'Chaotic' otherwise?
---
+
 
 -- Table Schema:
 -- Table: evening_tasks
