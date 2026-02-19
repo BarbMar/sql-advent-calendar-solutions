07fbf91bdd92e226c2af95f0936f7def04b3e63b
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 19:01:19 +0100

    Update day-15.sql

--- day-15.sql
+++ day-15.sql
@@ -5,8 +5,7 @@
 -- Question:
 -- The Grinch is tracking his daily mischief scores to see how his behavior changes over time. Can you find how many points his score increased or decreased each day compared to the previous day?
 --
--- The Grinch is tracking his daily mischief scores to see how his behavior changes over time. Can you find how many points his score increased or decreased each day compared to the previous day?
---
+
 
 -- Table Schema:
 -- Table: grinch_mischief_log
