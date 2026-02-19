119509039d72e3b3ba25256e7fe1a84c0b6a0c7f
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 19:02:09 +0100

    Update day-18.sql

--- day-18.sql
+++ day-18.sql
@@ -5,8 +5,7 @@
 -- Question:
 -- Over the 12 days of her data challenge, Data Dawn tracked her daily quiz scores across different subjects. Can you find each subject's first and last recorded score to see how much she improved?
 --
--- Over the 12 days of her data challenge, Data Dawn tracked her daily quiz scores across different subjects. Can you find each subject's first and last recorded score to see how much she improved?
---
+
 
 -- Table Schema:
 -- Table: daily_quiz_scores
