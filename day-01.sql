913a41e27e7a08356f1d1d44603f8b9f10b5a2b9
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 18:56:00 +0100

    Update day-01.sql

--- day-01.sql
+++ day-01.sql
@@ -5,8 +5,7 @@
 -- Question:
 -- Every year, the city of Whoville conducts a Reindeer Run to find the best reindeers for Santa's Sleigh. Write a query to return the name and rank of the top 7 reindeers in this race.
 --
--- Every year, the city of Whoville conducts a Reindeer Run to find the best reindeers for Santa's Sleigh. Can you write a query to return the name and rank of the top 7 reindeers in this race?
---
+
 
 -- Table Schema:
 -- Table: reindeer_run_results
