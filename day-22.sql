293baaa6c7d0b1407ef1e3aac409e43c1fdf0cf5
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 19:03:16 +0100

    Update day-22.sql

--- day-22.sql
+++ day-22.sql
@@ -5,8 +5,7 @@
 -- Question:
 -- The penguins are signing up for a community sleigh ride, but the organizers need a list of everyone who did NOT choose the "Evening Ride." How would you return all penguins whose selected time is not the evening slot?
 --
--- The penguins are signing up for a community sleigh ride, but the organizers need a list of everyone who did NOT choose the "Evening Ride." How would you return all penguins whose selected time is not the evening slot?
---
+
 
 -- Table Schema:
 -- Table: sleigh_ride_signups
