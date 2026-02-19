74dc7400e23d8c13222dd23d0a86a8f6c11e4665
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 19:02:43 +0100

    Update day-20.sql

--- day-20.sql
+++ day-20.sql
@@ -5,8 +5,7 @@
 -- Question:
 -- Jack Frost wants to review all the cocoa breaks he actually took — including the cocoa type and the location he drank it in. How would you combine the necessary tables to show each logged break with its matching cocoa details and location?
 --
--- Jack Frost wants to review all the cocoa breaks he actually took — including the cocoa type and the location he drank it in. How would you combine the necessary tables to show each logged break with its matching cocoa details and location?
---
+
 
 -- Table Schema:
 -- Table: cocoa_logs
