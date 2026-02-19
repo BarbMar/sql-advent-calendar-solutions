d7dfb872a385bd80c7469255a20eed46230f0a89
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 18:59:27 +0100

    Update day-09.sql

--- day-09.sql
+++ day-09.sql
@@ -5,8 +5,7 @@
 -- Question:
 -- The elves are testing new tinsel–light combinations to find the next big holiday trend. Write a query to generate every possible pairing of tinsel colors and light colors, include in your output a column that combines the two values separated with a dash ("-").
 --
--- The elves are testing new tinsel–light combinations to find the next big holiday trend. Write a query to generate every possible pairing of tinsel colors and light colors, include in your output a column that combines the two values separated with a dash ("-"). 
---
+
 
 -- Table Schema:
 -- Table: tinsel_colors
