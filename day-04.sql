e005d31f3bb1fc77d31d6e9c1caa7518cd690a8f
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 18:57:57 +0100

    Update day-04.sql

--- day-04.sql
+++ day-04.sql
@@ -5,8 +5,6 @@
 -- Question:
 -- Kevin's trying to decorate the house without sending the electricity bill through the roof. Write a query to find the top 5 most energy-efficient decorations (i.e. lowest cost per hour to operate).
 --
--- Kevin's trying to decorate the house without sending the electricity bill through the roof. Write a query to find the top 5 most energy-efficient decorations (i.e. lowest cost per hour to operate).
---
 
 -- Table Schema:
 -- Table: hall_decorations
