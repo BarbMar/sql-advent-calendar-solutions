54be0316f5a699d15351f202d9a39eb2c49b5dc1
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 19:03:33 +0100

    Update day-23.sql

--- day-23.sql
+++ day-23.sql
@@ -5,8 +5,7 @@
 -- Question:
 -- The Gingerbread House Competition wants to feature the top 3 builders who used the most distinct candy types in their designs. How would you find the builders with the highest count of unique candies, and return only the top three?
 --
--- The Gingerbread House Competition wants to feature the builders who used at least 4 distinct candy types in their designs. How would you identify these builders?
---
+
 
 -- Table Schema:
 -- Table: gingerbread_designs
