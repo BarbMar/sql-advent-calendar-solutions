5642450168f788881a786515411cb36537052c4e
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 18:57:11 +0100

    Update day-02.sql

--- day-02.sql
+++ day-02.sql
@@ -5,8 +5,6 @@
 -- Question:
 -- Santa wants to analyze which toys that were produced in his workshop have already been delivered to children. You are given two tables on toy production and toy delivery — can you return the toy_id of the toys that have been delivered?
 --
--- Santa wants to analyze which toys that were produced in his workshop have already been delivered to children. You are given two tables on toy production and toy delivery — can you return the toy ID and toy name of the toys that have been delivered?
---
 
 -- Table Schema:
 -- Table: toy_production
