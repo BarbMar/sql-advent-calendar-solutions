84014f2984715162bcf59fa53497a52a08f58183
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 18:57:35 +0100

    Update day-03.sql

--- day-03.sql
+++ day-03.sql
@@ -5,8 +5,7 @@
 -- Question:
 -- The Grinch has brainstormed a ton of pranks for Whoville, but he only wants to keep the top prank per target, with the highest evilness score. Return the most evil prank for each target. If two pranks have the same evilness, the more recently brainstormed wins.
 --
--- The Grinch has brainstormed a ton of pranks for Whoville, but he only wants to keep the top prank per target, with the highest evilness score. Return the most evil prank for each target. If two pranks have the same evilness, the more recently brainstormed wins.
---
+
 
 -- Table Schema:
 -- Table: grinch_prank_ideas
