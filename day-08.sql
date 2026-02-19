0888d0aa3ae17f8fed9eab12d789965c9fc9205f
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 18:59:11 +0100

    Update day-08.sql

--- day-08.sql
+++ day-08.sql
@@ -5,8 +5,7 @@
 -- Question:
 -- Mrs. Claus is organizing the holiday storage room and wants a single list of all decorations — both Christmas trees and light sets. Write a query that combines both tables and includes each item's name and category.
 --
--- Mrs. Claus is organizing the holiday storage room and wants a single list of all decorations — both Christmas trees and light sets. Write a query that combines both tables and includes each item's name and category.
---
+
 
 -- Table Schema:
 -- Table: storage_trees
