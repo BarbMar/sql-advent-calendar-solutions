0ef511f1d986b6c23a4d5b2c3596a398cb792424
Barbara Maria Hermanowska <114854839+BarbMar@users.noreply.github.com>
2026-02-19 19:01:02 +0100

    Update day-14.sql

--- day-14.sql
+++ day-14.sql
@@ -5,8 +5,7 @@
 -- Question:
 -- The Productivity Club is tracking members' challenge start dates and wants to calculate each member's focus_end_date, exactly 14 days after their start date. Can you write a query to return the existing table with the focus_end_date column?
 --
--- The Productivity Club is tracking members' challenge start dates and wants to calculate each member's focus_end_date, exactly 14 days after their start date. Can you write a query to return the existing table with the focus_end_date column?
---
+
 
 -- Table Schema:
 -- Table: focus_challenges
